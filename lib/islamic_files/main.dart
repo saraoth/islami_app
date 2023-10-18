441bbc50c3ca5183344a9eccaa24c69bd3c71c97
sarah <teachersarahothman2411@gmail.com>
2023-10-18 20:31:08 +0300

    initial project. added home screen.

---
+++ lib/islamic_files/main.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+import 'package:islamiapp/islamic_files/Home_Screen.dart';
+
+void main() {
+  runApp(Myapp());
+}
+
+class Myapp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      initialRoute: HomeScreen.routeName,
+      routes: {
+        HomeScreen.routeName: (context) => HomeScreen(),
+      },
+    );
+  }
+}
